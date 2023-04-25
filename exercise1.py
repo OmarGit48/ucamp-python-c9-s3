3a264294f509691e2a38b748c4e834aea3e1d7f5
Omar Ontiveros <ontiveros2000@hotmail.com>
2023-04-25 02:18:35 -0600

    agregar codigo y calculadora

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+numero = input("Digite un número: ")
+print (numero)
+print (type(numero))
+
+numeroentero = int(numero)
+print (type(numeroentero))
+
+numero2 = numeroentero/3
+print("numero2", numero2)
+
+print (numero2 > 2)

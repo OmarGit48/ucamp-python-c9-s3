ae917ceb7ba2250d0e6777cfa47428d4f5740df4
Omar Ontiveros <ontiveros2000@hotmail.com>
2023-04-26 20:56:42 -0600

    secion terminada de clase de miercoles ya pensando en como sera lo demas del codigo

---
+++ condicionante.py
@@ -0,0 +1,16 @@
+
+# Solicita el usuario un numero
+numero = input("Digita un Númuero: ")
+numero = int(numero)
+
+#si el numero es par
+if numero % 2 == 0:
+    # Imprime "El numero {numero} es par"
+    print (f"El número {numero} es par")
+ 
+#Si el numero es Impar
+# elif numero % 2 != 0:
+else:
+    # Imprime "El numero {numero} es Impar"
+    print (f"El número es {numero} es impar")
+    
